6adcf6d1c049e1d29db90d5a8bf3a837d02c72f5
fakhtehpanah <fakhipanah@gmail.com>
2025-10-06 15:23:13 +0330

    create trending API

---
+++ my-app/src/pages/api/trending.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    try {
+      const response = await fetch("https://api.coingecko.com/api/v3/search/trending");
+      const data = await response.json();
+
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+}
